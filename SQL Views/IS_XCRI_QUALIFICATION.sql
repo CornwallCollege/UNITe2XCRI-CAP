d8dfff45deaca34c76b5a373bf14c116e61202b0
m.trebilcock <mike.trebilcock@cornwall.ac.uk>
2012-12-04 15:35:54 +0000

    Improved Qualification SQL View

--- SQL Views/IS_XCRI_QUALIFICATION.sql
+++ SQL Views/IS_XCRI_QUALIFICATION.sql
@@ -2,14 +2,18 @@ CREATE OR REPLACE FORCE VIEW "UNITE"."IS_XCRI_QUALIFICATION" ("MasterCourseID", 
 AS
   SELECT DISTINCT t1.m_id "MasterCourseID",
     t2.vc_name "abbr",
-    NULL                          AS "accreditedBy",
+    case when   t1.m_externalaward is null then null else 'Plymouth University' end  AS "accreditedBy",
     t3.AWARDING_ORGANISATION_DESC AS "awardedBy",
     NULL                          AS "description",
     NULL                          AS "descriptionprofessionalStatus",
-    NULL                          AS "educationLevel",
-    NULL                          AS "identifier",
-    NULL                          AS "title",
-    NULL                          AS "url"
+    t1.m_level                     AS "educationLevel",
+    nvl(t8.mi_q02m02,t1.m_externalaward) AS "identifier",
+    nvl(t9.learning_aim_title, t1.m_name)AS "title",
+    case when t8.mi_q02m02 is not null AND substr(t8.mi_q02m02,1,1) not in ('Z','C')
+    then
+    'http://register.ofqual.gov.uk/Qualification/Details/'||substr(t8.mi_q02m02,1,3)||'_'||substr(t8.mi_q02m02,4,4)||'_'||substr(t8.mi_q02m02,8,1)
+    else
+    null end AS "url"
   FROM capd_moduleisr t8,
     caps_valid_codes t2,
     capd_moduletemplate t5,
